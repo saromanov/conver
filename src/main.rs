04e3827be2c806319f1017f1d2046bc162db2445
Sergey <xxsmotur@gmail.com>
2019-08-29 22:12:16 +0500

    reading of the first argument of the file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use std::error::Error;
 use std::io;
 use std::process;
+use std::ffi::OsString;
+use std::fs::File;
+use std::env;
 
 fn read_csv() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.records() {.
+    for result in rdr.records() {
         let record = result?;
         println!("{:?}", record);
     }
     Ok(())
 }
 
+// returns file name of the 
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
 
 fn main() {
+    
+    let file_name = get_first_arg();
    if let Err(err) = read_csv() {
         println!("error running example: {}", err);
         process::exit(1);
