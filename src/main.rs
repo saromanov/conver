806823db4fcecdbce3543227af64fc1ed7cb0fb2
Sergey <xxsmotur@gmail.com>
2019-08-30 07:55:35 +0500

    getting file name from arguments

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::ffi::OsString;
 use std::fs::File;
 use std::env;
 
-fn read_csv() -> Result<(), Box<dyn Error>> {
+fn read_csv(filename:String) -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
     for result in rdr.records() {
         let record = result?;
@@ -24,8 +24,8 @@ fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
 
 fn main() {
     
-    let file_name = get_first_arg();
-   if let Err(err) = read_csv() {
+    let file_name = get_first_arg().unwrap();
+   if let Err(err) = read_csv(file_name.into_string().unwrap()) {
         println!("error running example: {}", err);
         process::exit(1);
     }
