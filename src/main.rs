c8cae5a718ed997176e4955f86a94ce432510278
Sergey <xxsmotur@gmail.com>
2019-09-07 18:57:11 +0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,11 @@ impl App {
     pub fn sql_generate(&self) {
         let data = get_str_from_string_record(self.headers.clone());
         let mut result = format!("INSERT INTO({}) VALUES()",  data.as_str());
+
+        for v in self.rows.clone() {
+            let d = format!("({})", get_str_from_string_record(v));
+            println!("{:?}", d);
+        }
         println!("{:?}", result)
     }
 }
